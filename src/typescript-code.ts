9725c8938e14541ae1a0cc0ecda8fa64e67f99ea
Carl Fürstenberg <azatoth@gmail.com>
2019-11-13 20:55:23 +0100

    Prevent rebuilding if nothing has changed in the lambda code
    Also remove old build when actually rebuilding

--- src/typescript-code.ts
+++ src/typescript-code.ts
@@ -1,17 +1,17 @@
-import { Construct } from '@aws-cdk/core'
-import { Code, AssetCode } from '@aws-cdk/aws-lambda'
-import * as pathModule from 'path'
-import * as child_process from 'child_process'
-import * as fs from 'fs'
-
-const typeScriptAlreadyBuilt: string[] = [] // list of source code paths already built in this session
+import { AssetCode, Code } from "@aws-cdk/aws-lambda";
+import { Construct } from "@aws-cdk/core";
+import * as child_process from "child_process";
+import * as fs from "fs";
+import * as pathModule from "path";
+import * as recursive from "recursive-readdir";
+const typeScriptAlreadyBuilt: string[] = []; // list of source code paths already built in this session
 
 /**
  * Wrapper for the Code abstract class, which provides some static helper methods.
  */
 export abstract class TypeScriptCode extends Code {
   public static asset(path: string): TypeScriptAssetCode {
-    return new TypeScriptAssetCode(path)
+    return new TypeScriptAssetCode(path);
   }
 }
 
@@ -19,81 +19,135 @@ export abstract class TypeScriptCode extends Code {
  * Extension for AssetCode to run a TypeScript build step before deployment.
  */
 export class TypeScriptAssetCode extends AssetCode {
-  private typeScriptSourcePath: string // original source code path
+  private typeScriptSourcePath: string; // original source code path
 
-  constructor(path: string) {
+  public constructor(path: string) {
     // Add a .deploy subfolder which contains the built files and is deployed to S3
-    super(pathModule.join(path, '.deploy'))
+    super(pathModule.join(path, ".deploy"));
     // Remember the original source folder
-    this.typeScriptSourcePath = path
+    this.typeScriptSourcePath = path;
   }
-
   public bind(construct: Construct) {
-    this.typeScriptBuild()
-    return super.bind(construct)
+    this.typeScriptBuild();
+    return super.bind(construct);
   }
 
-  private typeScriptBuild() {
+  private async typeScriptBuild() {
     // Keep track of which folders have already been built
     if (typeScriptAlreadyBuilt.includes(this.typeScriptSourcePath)) {
-      return
+      return;
     }
-    typeScriptAlreadyBuilt.push(this.typeScriptSourcePath)
+    typeScriptAlreadyBuilt.push(this.typeScriptSourcePath);
 
-    // Ensure the deploy path exists
+    // Only continue with build if we have something new to build
+    const stamp = pathModule.join(this.path, ".stamp");
+    if (fs.existsSync(stamp)) {
+      const mtime = fs.statSync(stamp).mtime;
+      const newer_files = await recursive(this.typeScriptSourcePath, [
+        (file, stats) => {
+          return stats.isDirectory() || stats.mtime <= mtime;
+        },
+      ]);
+
+      if (newer_files.length === 0) {
+        // nothing new to build
+        return;
+      }
+    }
+
+    // Clean out current build
     try {
-      fs.mkdirSync(this.path)
+      if (fs.existsSync(this.path)) fs.unlinkSync(this.path);
+      fs.mkdirSync(this.path);
     } catch (err) {
       // ignore errors
     }
-
     // Run the TypeScript compiler in our own module path, so that our own dependency is used
-    const tscChild = child_process.spawnSync('npx', ['tsc', '--project', this.typeScriptSourcePath, '--outDir', this.path], {
-      cwd: __dirname,
-      stdio: 'inherit',
-    })
+    const tscChild = child_process.spawnSync(
+      "npx",
+      ["tsc", "--project", this.typeScriptSourcePath, "--outDir", this.path],
+      {
+        cwd: __dirname,
+        stdio: "inherit",
+      }
+    );
     if (tscChild.error) {
-      throw tscChild.error
+      throw tscChild.error;
     }
     if (tscChild.status !== 0) {
-      throw new Error('TypeScript compiler error: ' + tscChild.status)
+      throw new Error("TypeScript compiler error: " + tscChild.status);
     }
 
     function readFileSyncOrNull(filepath: string, encoding: string) {
       try {
-        return fs.readFileSync(filepath, encoding)
+        return fs.readFileSync(filepath, encoding);
       } catch (err) {
-        if (err.code === 'ENOENT') return null
-        else throw err
+        if (err.code === "ENOENT") return null;
+        else throw err;
       }
     }
 
     // Run NPM package install so that the Lambda function gets all dependencies - unless we've already run it
 
     // New versions in source path
-    const newPackageLockData = readFileSyncOrNull(pathModule.join(this.typeScriptSourcePath, 'package-lock.json'), 'utf8')
-    const newPackageData = readFileSyncOrNull(pathModule.join(this.typeScriptSourcePath, 'package.json'), 'utf8')
+    const newPackageLockData = readFileSyncOrNull(
+      pathModule.join(this.typeScriptSourcePath, "package-lock.json"),
+      "utf8"
+    );
+    const newPackageData = readFileSyncOrNull(
+      pathModule.join(this.typeScriptSourcePath, "package.json"),
+      "utf8"
+    );
 
     // Old versions in deploy path (if any)
-    const oldPackageLockData = readFileSyncOrNull(pathModule.join(this.path, 'package-lock.json'), 'utf8')
-    const oldPackageData = readFileSyncOrNull(pathModule.join(this.path, 'package.json'), 'utf8')
+    const oldPackageLockData = readFileSyncOrNull(
+      pathModule.join(this.path, "package-lock.json"),
+      "utf8"
+    );
+    const oldPackageData = readFileSyncOrNull(
+      pathModule.join(this.path, "package.json"),
+      "utf8"
+    );
 
-    if (newPackageData && (newPackageData !== oldPackageData || newPackageLockData !== oldPackageLockData)) {
+    if (
+      newPackageData &&
+      (newPackageData !== oldPackageData ||
+        newPackageLockData !== oldPackageLockData)
+    ) {
       // We have a package.json, and either package.json or package-lock.json has changed since last build, or no build done yet
-      fs.writeFileSync(pathModule.join(this.path, 'package-lock.json'), newPackageLockData, 'utf8')
-      fs.writeFileSync(pathModule.join(this.path, 'package.json'), newPackageData, 'utf8')
+      fs.writeFileSync(
+        pathModule.join(this.path, "package-lock.json"),
+        newPackageLockData,
+        "utf8"
+      );
+      fs.writeFileSync(
+        pathModule.join(this.path, "package.json"),
+        newPackageData,
+        "utf8"
+      );
 
       // Execute npm install
-      const npmChild = child_process.spawnSync('npm', ['install', '--production'], {
-        cwd: this.path,
-        stdio: 'inherit',
-      })
+      const npmChild = child_process.spawnSync(
+        "npm",
+        ["install", "--production"],
+        {
+          cwd: this.path,
+          stdio: "inherit",
+        }
+      );
       if (npmChild.error) {
-        throw npmChild.error
+        throw npmChild.error;
       }
       if (npmChild.status !== 0) {
-        throw new Error('TypeScript compiler error: ' + npmChild.status)
+        throw new Error("TypeScript compiler error: " + npmChild.status);
       }
+    }
+    // Touch the stamp file
+    const time = new Date();
+    try {
+      fs.utimesSync(stamp, time, time);
+    } catch (err) {
+      fs.closeSync(fs.openSync(stamp, "w"));
     }
   }
 }
